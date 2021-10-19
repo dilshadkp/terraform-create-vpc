929f8a36df8946f87666b0a5e99fadc98c5c998e
dilshadkp <91471256+dilshadkp@users.noreply.github.com>
2021-10-19 18:42:09 +0530

    Update mywork.tf

--- mywork.tf
+++ mywork.tf
@@ -161,6 +161,45 @@ resource "aws_route_table" "private" {
   }
 }
 
+####################################################
+# public route table association
+####################################################
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public3" {
+  subnet_id      = aws_subnet.public3.id
+  route_table_id = aws_route_table.public.id
+}
+
+####################################################
+# private route table association
+####################################################
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private3" {
+  subnet_id      = aws_subnet.private3.id
+  route_table_id = aws_route_table.private.id
+}
+
+
 ######################################
 #key pair
 ######################################
